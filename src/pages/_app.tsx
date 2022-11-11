e81b4f07303e1138697eec787a335ad525fc9b19
Griffin Baxter <griffinrbaxter@gmail.com>
2022-11-11 22:39:35 +1300

    Added NextUI and basic navigation bar.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,14 @@ import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 
+import { NextUIProvider } from '@nextui-org/react';
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
