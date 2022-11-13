bcf1e005c03b04ae3e4342bf4f3774db6b99ac88
Griffin Baxter <griffinrbaxter@gmail.com>
2022-11-13 21:28:14 +1300

    Updated project card with attributes and tag colours.

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,22 +1,56 @@
-import { Card } from "flowbite-react";
+import {Badge, Card} from "flowbite-react";
+import { type NextPage } from "next";
 
-export default function ProjectCard() {
+export enum Language {
+    C_CPlusPlus,
+    HTML_CSS,
+    Java,
+    JavaScript_TypeScript,
+    Python,
+    SQL,
+}
+
+interface LanguageData {
+    name: string,
+    badgeColour: string,
+}
+
+const languageMap: Record<Language, LanguageData> = {
+    [Language.C_CPlusPlus]: {name: "C / C++", badgeColour: "purple"},
+    [Language.HTML_CSS]: {name: "HTML / CSS", badgeColour: "pink"},
+    [Language.Java]: {name: "Java", badgeColour: "failure"},
+    [Language.JavaScript_TypeScript]: {name: "JavaScript / TypeScript", badgeColour: "warning"},
+    [Language.Python]: {name: "Python", badgeColour: "info"},
+    [Language.SQL]: {name: "SQL", badgeColour: "success"},
+}
+
+interface Props {
+    projectName: string,
+    projectDate: string,
+    projectLanguages: Language[],
+}
+
+const ProjectCard: NextPage<Props> = ({projectName: name, projectDate: date, projectLanguages: languages}) => {
     return (
         <Card
             imgSrc="/logo.png"
             imgAlt="Image"
         >
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Project Name
+                {name}
             </h5>
             <h6 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Date
+                {date}
             </h6>
-            <div>
-                <span className="bg-red-100 text-red-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900">
-                    Language
-                </span>
+            <div className="flex flex-wrap gap-2">
+                {languages.map((language) => (
+                    <Badge key={language} color={languageMap[language].badgeColour} size="sm">
+                        {languageMap[language].name}
+                    </Badge>
+                ))}
             </div>
         </Card>
     )
 }
+
+export default ProjectCard
