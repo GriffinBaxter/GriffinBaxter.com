3b0c36a488a9c32ebb898123f350bc2123fabf56
Griffin Baxter <griffinrbaxter@gmail.com>
2022-11-23 22:38:25 +1300

    Styled remaining content HTML tags (h4, lists, quotes).

--- src/components/post/post-content.tsx
+++ src/components/post/post-content.tsx
@@ -22,15 +22,40 @@ function styleLinks(html: string) {
     )
 }
 
+function styleH4(html: string) {
+    const isRating = html.endsWith("/10")
+    if (isRating) {
+        return `<p class="text-3xl font-bold text-center py-6">${styleLinks(html)}</p>`
+    } else {
+        return `<p class="text-3xl font-bold">${styleLinks(html)}</p>`
+    }
+}
+
+function styleList(html: string) {
+    return html.replaceAll("<li>", `<li class="text-xl pb-2">`)
+}
+
+function styleQuote(html: string) {
+    return `
+        <div class="p-8 border-l-2 border-blue-500">
+            ${styleLinks(html.replaceAll("<p>", `<p class="text-xl pb-8">`))}
+        </div>
+    `
+}
+
 const PostContent: NextPage<Props> = ({ blocks }) => {
     let contentHTML = ""
     if (blocks) {
-        for (const block of blocks) { // TODO: cover all tag scenarios
+        for (const block of blocks) {
             contentHTML += `<p class="text-xl pt-8">`
             if (block.tagName == "p") {
                 contentHTML += styleLinks(block.innerHtml)
             } else if (block.tagName == "h4") {
-                contentHTML += styleLinks(block.innerHtml)  // TODO: update h4 styling
+                contentHTML += styleH4(block.innerHtml)
+            } else if (block.tagName == "ul") {
+                contentHTML += styleList(block.innerHtml)
+            } else if (block.tagName == "blockquote") {
+                contentHTML += styleQuote(block.innerHtml)
             } else if (block.tagName == "hr") {
                 contentHTML += divider()
             } else if (block.tagName == "figure") {
