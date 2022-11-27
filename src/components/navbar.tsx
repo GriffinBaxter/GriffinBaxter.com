37f8f42659a6fc7ffcfd12487f3a840e2629cd52
Griffin Baxter <griffinrbaxter@gmail.com>
2022-11-27 15:27:02 +1300

    Updated navbar styling using Flowbite React's theming.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,4 @@
 import { Navbar } from "flowbite-react";
-import Image from "next/image";
-import logo from "/public/logo.png"
 import { type NextPage } from "next";
 
 export enum NavigationPage {
@@ -16,21 +14,22 @@ interface Props {
 
 const NavBar: NextPage<Props> = ({page: navPage}) => {
     return (
-        <Navbar className="mx-auto max-w-[1200px]" fluid={true} rounded={true}>
-            <Navbar.Brand href="/">
-                <Image src={logo} className="mr-3 h-6 w-6 sm:h-9 sm:w-9" alt="Logo"/>
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+        <div className="w-full bg-black mb-10">
+            <Navbar className="mx-auto max-w-[1200px] bg-black" fluid={true} >
+                <Navbar.Brand href="/">
+                <span className="self-center whitespace-nowrap text-xl font-bold text-white">
                     Griffin Baxter
                 </span>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse>
-                <Navbar.Link href="/" active={navPage == NavigationPage.Home}>Home</Navbar.Link>
-                <Navbar.Link href="projects" active={navPage == NavigationPage.SoftwareProjects}>Software Projects</Navbar.Link>
-                <Navbar.Link href="game-reviews" active={navPage == NavigationPage.GameReviews}>Game Reviews</Navbar.Link>
-                <Navbar.Link href="contact" active={navPage == NavigationPage.Contact}>Contact</Navbar.Link>
-            </Navbar.Collapse>
-        </Navbar>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Navbar.Link href="/" active={navPage == NavigationPage.Home}>Home</Navbar.Link>
+                    <Navbar.Link href="projects" active={navPage == NavigationPage.SoftwareProjects}>Software Projects</Navbar.Link>
+                    <Navbar.Link href="game-reviews" active={navPage == NavigationPage.GameReviews}>Game Reviews</Navbar.Link>
+                    <Navbar.Link href="contact" active={navPage == NavigationPage.Contact}>Contact</Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
     )
 }
 
