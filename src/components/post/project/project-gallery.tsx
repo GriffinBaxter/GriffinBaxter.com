c709d7cfe12443d41aab9c9e55d402089c9ab4ae
Griffin Baxter <GriffinRBaxter@gmail.com>
2025-08-26 17:07:49 +1200

    WEB-19: Add number buttons to project galleries

--- src/components/post/project/project-gallery.tsx
+++ src/components/post/project/project-gallery.tsx
@@ -9,54 +9,73 @@ interface Props {
 
 export default function ProjectGallery({ blocks }: Props) {
   return (
-    <div className="carousel w-full pt-6">
-      {blocks.map((block, slideNumber) => {
-        return (
-          <div
-            id={`slide${slideNumber.toString()}`}
-            key={block.innerHtml}
-            className="carousel-item relative aspect-16/9 w-full"
-          >
-            <Image
-              className="mx-auto max-h-[675px]"
-              src={
-                process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID
-                  ? `https://${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}.ufs.sh/f/${block.innerHtml}`
-                  : ""
-              }
-              alt={`Gallery Image (Slide ${(slideNumber + 1).toString()})`}
-              fill={true}
-              style={{ objectFit: "contain" }}
-            />
-            <div className="absolute top-1/2 right-5 left-5 flex -translate-y-1/2 transform justify-between">
-              <button
-                className="btn btn-circle"
-                onClick={() => {
-                  document
-                    .getElementById(
-                      `slide${slideNumber > 0 ? (slideNumber - 1).toString() : (blocks.length - 1).toString()}`,
-                    )
-                    ?.scrollIntoView({ behavior: "instant" });
-                }}
-              >
-                ❮
-              </button>
-              <button
-                className="btn btn-circle"
-                onClick={() => {
-                  document
-                    .getElementById(
-                      `slide${slideNumber < blocks.length - 1 ? (slideNumber + 1).toString() : "0"}`,
-                    )
-                    ?.scrollIntoView({ behavior: "instant" });
-                }}
-              >
-                ❯
-              </button>
+    <>
+      <div className="carousel w-full pt-6">
+        {blocks.map((block, slideNumber) => {
+          return (
+            <div
+              id={`slide${slideNumber.toString()}`}
+              key={block.innerHtml}
+              className="carousel-item relative aspect-16/9 w-full"
+            >
+              <Image
+                className="mx-auto max-h-[675px]"
+                src={
+                  process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID
+                    ? `https://${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}.ufs.sh/f/${block.innerHtml}`
+                    : ""
+                }
+                alt={`Gallery Image (Slide ${(slideNumber + 1).toString()})`}
+                fill={true}
+                style={{ objectFit: "contain" }}
+              />
+              <div className="absolute top-1/2 right-5 left-5 flex -translate-y-1/2 transform justify-between">
+                <button
+                  className="btn btn-circle"
+                  onClick={() => {
+                    document
+                      .getElementById(
+                        `slide${slideNumber > 0 ? (slideNumber - 1).toString() : (blocks.length - 1).toString()}`,
+                      )
+                      ?.scrollIntoView({ behavior: "instant" });
+                  }}
+                >
+                  ❮
+                </button>
+                <button
+                  className="btn btn-circle"
+                  onClick={() => {
+                    document
+                      .getElementById(
+                        `slide${slideNumber < blocks.length - 1 ? (slideNumber + 1).toString() : "0"}`,
+                      )
+                      ?.scrollIntoView({ behavior: "instant" });
+                  }}
+                >
+                  ❯
+                </button>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+      <div className="flex w-full flex-wrap justify-center gap-2 py-2">
+        {blocks.map((block, slideNumber) => {
+          return (
+            <button
+              key={block.innerHtml}
+              className="btn btn-xs"
+              onClick={() => {
+                document
+                  .getElementById(`slide${slideNumber.toString()}`)
+                  ?.scrollIntoView({ behavior: "instant" });
+              }}
+            >
+              {(slideNumber + 1).toString()}
+            </button>
+          );
+        })}
+      </div>
+    </>
   );
 }
